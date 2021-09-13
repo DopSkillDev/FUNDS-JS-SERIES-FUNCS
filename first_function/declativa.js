b1416f6cf64c2ba1b1a701ea19ff410a7805a248
DaniloOPinheiro <danilopinheiro88@icloud.com>
2021-09-13 11:49:53 -0300

    add novas linhas

--- first_function/declativa.js
+++ first_function/declativa.js
@@ -1,45 +1,43 @@
 // OBS: o nome inserido na função é só para dar uma melhor visão. 
 // No entanto não é uma boa prática o nome, melhor ideia de nome que seja no singular.
 
-// Função de forma simples
+console.log("--- Função de forma simples ---")
 function MeDeclara() {
   console.log('Me Declarando')
 }
 
 MeDeclara()
 
-// Função com parâmetro, utilizando de duas formas.
+console.log("--- Função com parâmetro, utilizando de duas formas. ---")
 function MeDeclaraComParametroConcatenado(ComParametro) {
   // 1 - Concatenado
   console.log('Me Declarando com ' + ComParametro)
 }
 
-MeDeclaraComParametroConcatenado()
+MeDeclaraComParametroConcatenado('Com Parâmetro')
 
 function MeDeclaraComParametroTemplateString(ComParametro) {
   // 2 - Template String
   console.log(`Me Declarando com ${ComParametro}`)
 }
 
-MeDeclaraComParametroTemplateString()
+MeDeclaraComParametroTemplateString('Com Parâmetro')
 
-// Função com retorno, utilizando algumas formas.
+console.log("--- Função com retorno, utilizando algumas formas.  ---")
 function MeDeclaraComRetorno() {
   return 'Me Retorne'
 }
 
-MeDeclaraComRetorno()
-
-// É possível utilizar três formas para a chamada do retorno.
+console.log("--- É possível utilizar três formas para a chamada do retorno.")
 var MeDeclaraComVar = MeDeclaraComRetorno()
 console.log(MeDeclaraComVar)
 let MeDeclaraComLet =  MeDeclaraComRetorno()
 console.log(MeDeclaraComLet)
 const MeDeclaraComConst = MeDeclaraComRetorno()
 
-// Função com as duas declarativas.
+console.log("--- Função com as duas declarativas. ---")
 function MeDeclaraComParametroComRetorno(ComParametro, ComRetorno) {
    return `Me Declarando com ${ComParametro} e com ${ComRetorno}`
 }
 
-console.log(MeDeclaraComParametroComRetorno('Parametros', 'Retornos'))
+console.log(MeDeclaraComParametroComRetorno('Parâmetros', 'Retornos'))
